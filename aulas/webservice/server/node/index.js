9d1e9553cec778625c0015e38f8ffbf5e5f820a8
Wancharle Quirino <wancharle.quirino@wowtec.com.br>
2017-10-25 15:17:29 -0200

    adicionado suporte acesso externo do webservice

--- aulas/webservice/server/node/index.js
+++ aulas/webservice/server/node/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 
+function liberaAcesso(res){
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header( 'Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+}
+
 function paginaInicial(req, res) {
+      liberaAcesso(res)
       res.send('Olá mundo!')
 }
 
--- breakout/www/js/sons.js
+++ breakout/www/js/sons.js
@@ -1,7 +1,7 @@
 window.sons = {}; // dicionario global de sons
 
 function loadMedia (url){
-    var u = ""+url;
+    var u = "file:///android_asset/"+url;
     var media = new Media(u,
              function () { console.log("playAudio():Audio Success"); },
              function (err) { console.log("playAudio():Audio Error: "); console.log( err); }
