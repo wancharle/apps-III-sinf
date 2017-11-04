bc1b5ee8c2dd1a3f5a77c66868f361ea1e310bc3
Wancharle Quirino <wancharle.quirino@wowtec.com.br>
2017-11-03 23:10:42 -0200

    adicionado acesso a banco de dados nosql

---
+++ aulas/banco-de-dados/servidor/nosql/crud.js
@@ -0,0 +1,77 @@
+// inporta biblioteca para conectar ao servidor nosql mongodb
+var MongoClient = require('mongodb').MongoClient
+
+// função para facilitar a abertura do banco nosql
+function openNOSQLdb(nomeDoBanco){
+	var urlBanco = 'mongodb://localhost/'+nomeDoBanco;
+	// cria uma 'Promessa' que é um padrão que ajuda no desenvolvimento assincrono e evita o "Callback hell"
+    return new Promise(function(resolve, reject){
+        MongoClient.connect(urlBanco, function(err, db){
+            if (err) {
+                reject(err)
+            } else {
+                resolve(db)
+            }
+        })
+    })
+}
+
+
+var banco = null;
+var colecaoUsuarios = null;
+
+// conecta ao bancoteste ou cria um banco com esse nome se ele nao existir
+openNOSQLdb("bancoteste") 
+	.then(function (db){
+		// salva-se uma copia da variavel db pois ela será usada depois em outro contexto
+		banco = db;
+
+		// Obtem ou cria uma coleção usuarios (uma coleção em nosql é similar ao conceito de uma tabela em sql)
+		// observe que não foi necessario definir a estrutura de campos/colunas da coleção como
+		// seria necessário para uma tabela de um banco sql
+		colecaoUsuarios = db.collection('usuarios')
+		return colecaoUsuarios 
+	})
+	.then(function (colecaoUsuarios){
+		// cria uma lista com dados de  10 usuarios de exemplo
+		var listaDeUsuarios = []
+		for (var i = 1; i <= 10; i++) {
+			 var data = new Date();
+			 var horario= data.toLocaleTimeString();
+			 listaDeUsuarios.push({id:i,horario:horario})
+		}
+
+		// insere os usuarios no banco atraves do metodo insertMany (que insere mais de um registro de uma vez)
+		// para inserir apenas um registro use a função insertOne em vez da insertMany
+		return colecaoUsuarios.insertMany(listaDeUsuarios)
+	})
+	.then(function(){ 
+		// remove o usuario 8
+		return colecaoUsuarios.deleteOne({id:8})
+	})
+	.then(function(){ 
+		// Atualiza o horario do usuario 9 para  '00:00:00'
+		return colecaoUsuarios.updateOne({id:9},{ $set:{horario:'00:00:00'}})
+	})
+	.then(function(){ 
+		// Realiza uma consulta para imprimir os usuarios cadastrados no banco
+		return colecaoUsuarios.find({})
+	})
+	.then(function(usuarios){ 
+		// imprime os usuarios encontrados no banco
+		usuarios.forEach(function(u){
+			console.log("Usuario id="+u.id, "data='"+u.horario+"'");                                                          
+		})
+
+		// remove a coleção usuarios (equivalente a um drop table)
+		return colecaoUsuarios.drop();
+	})
+	.then(function(){
+		//fecha conexao com o banco
+		return banco.close();
+	})
+	.catch(function (err){
+		console.log(err)
+	})
+
+
---
+++ aulas/banco-de-dados/servidor/nosql/readme.md
@@ -0,0 +1,26 @@
+
+# Dependências
+
+São basicamente duas dependências: um servidor nosql e uma biblioteca para acessá-lo.
+
+## Servidor nosql
+
+Para este exemplo instale o servidor mongodb.
+1. [Instalação do mongodb no ubuntu](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/)
+2. [Instalação do mongodb no windows](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/)
+
+
+## Biblioteca de acesso ao servidor
+
+Para instalar a biblioteca de acesso ao servidor mongodb digite o seguinte comando na pasta do projeto:
+
+```bash
+$ npm install --save mongodb
+```
+
+# Testes
+
+Para testar o código execute o comando abaixo
+```bash
+$ node crud.js
+```
