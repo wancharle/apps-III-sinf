112fe00ef4fe03346347795c27906c0ffac2e119
Wancharle Quirino <wancharle.quirino@wowtec.com.br>
2017-11-28 20:36:12 -0200

    funcionando bd

---
+++ aulas/banco-de-dados/cliente/cordova/bd/www/js/pessoa-service.js
@@ -0,0 +1,64 @@
+angular.module('PessoaService',[]).factory('PessoaService',['$http', function($http){
+
+   var service = {}
+    var db = null;
+   function cb_error(error){ 
+       console.log(error);
+       alert("Erro: "+error);
+   }
+
+
+   function inicializaBancoDeDados(){
+       db = window.sqlitePlugin.openDatabase({name: 'bancoCrud.db', location: 'default'});
+       db.transaction(function(tx){
+        tx.executeSql("CREATE TABLE IF NOT EXISTS pessoa (id INT unique not null, nome TEXT, dataNasc TEXT)")
+       },function(error){
+                console.log(error)
+                console.log("^^^^^^^^ ERROR BANCO ^^^^^^^")
+        },function(){ console.log("---> BANCO INICIALIZADO")}
+       )
+   }
+
+   function getPessoas(cb_sucesso){
+     db.transaction(function(tx){
+        tx.executeSql('SELECT * FROM pessoa',[],cb_sucesso,cb_error);
+     });
+   }
+
+   function getPessoa(id_pessoa,cb_sucesso){
+   }
+
+   function deletePessoa(id_pessoa,cb_sucesso){
+	   db.transaction(function(tx){
+            if (id_pessoa == "null" || id_pessoa == null)
+            tx.executeSql("DELETE FROM pessoa WHERE id IS NULL",[])
+            else
+            tx.executeSql("DELETE FROM pessoa WHERE id=?",[id_pessoa])
+       },cb_error,cb_sucesso)
+   }
+
+   function inserePessoa(id_pessoa,nome,data,cb_sucesso){
+       db.transaction(function(tx){
+            tx.executeSql("INSERT INTO pessoa VALUES (?,?,?)",[id_pessoa,nome,data]);
+       },cb_error,cb_sucesso)
+   }
+
+   function atualizaPessoa(id_pessoa,nome,data,cb_sucesso){
+   }
+
+   document.addEventListener('deviceready', function() {
+    inicializaBancoDeDados()
+   });
+
+
+   service.getPessoas = getPessoas 
+   service.getPessoa = getPessoa
+   service.deletePessoa = deletePessoa
+   service.inserePessoa = inserePessoa
+   service.atualizaPessoa = atualizaPessoa
+   service.inicializaBancoDeDados= inicializaBancoDeDados;
+   return service
+
+}]);
+
+
